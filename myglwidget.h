c115679300abcf66e611a4903ee546c9b51faa36
ChaCha-Chan <56464361+ChaCha-Chan@users.noreply.github.com>
2021-01-02 23:45:32 +0800

    Add files via upload

--- myglwidget.h
+++ myglwidget.h
@@ -16,7 +16,7 @@
 #include <sstream>
 #include <string>
 using namespace std;
-
+#define PI 3.1415926
 
 class MyGLWidget : public QOpenGLWidget, protected QOpenGLExtraFunctions {
     Q_OBJECT
@@ -28,8 +28,12 @@ public:
 protected:
     void initializeGL();
     void init_shaders(const char* v_path, const char* f_path);
+    void set_alpha(GLfloat alpha);
     void set_matrix();
-    void set_texture();
+    void draw_map();
+    void draw_roadside();
+    void draw_gate();
+    void r_t_set_matrix(GLfloat r0, GLfloat r1, GLfloat r2, GLfloat r3, GLfloat t1, GLfloat t2, GLfloat t3);
     void paintGL();
     void resizeGL(int width, int height);
 
