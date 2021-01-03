6786338fedeb6db10d093b64565723a1f3fe9a5e
ChaCha-Chan <56464361+ChaCha-Chan@users.noreply.github.com>
2021-01-04 03:38:43 +0800

    Add files via upload

--- myglwidget.h
+++ myglwidget.h
@@ -16,7 +16,7 @@
 #include <sstream>
 #include <string>
 using namespace std;
-
+#define PI 3.1415926
 
 class MyGLWidget : public QOpenGLWidget, protected QOpenGLExtraFunctions {
     Q_OBJECT
@@ -28,8 +28,20 @@ public:
 protected:
     void initializeGL();
     void init_shaders(const char* v_path, const char* f_path);
+    void set_alpha(GLfloat alpha);
     void set_matrix();
-    void set_texture();
+    void draw_map();
+    void draw_roadside();
+    void get_cylinder_v_i(GLfloat r, GLfloat h, GLuint u_num, GLfloat* temp_v, GLuint* temp_i);
+    void get_cycle_v_i(GLfloat r, GLuint u_num, GLfloat* temp_v, GLuint* temp_i);
+    void draw_gate();
+    void draw_buildings();
+    void draw_p(GLuint number);
+    void turn_p(GLuint number, GLint dir, GLfloat x, GLfloat z);
+    void move_p(GLuint number, GLint dir, GLfloat x, GLfloat z);
+    void r_t_set_matrix(GLfloat r0, GLfloat r1, GLfloat r2, GLfloat r3, GLfloat t1, GLfloat t2, GLfloat t3);
+    void s_r_t_set_matrix(GLfloat s1, GLfloat s2, GLfloat s3, GLfloat r0, GLfloat r1, GLfloat r2, GLfloat r3, GLfloat t1, GLfloat t2, GLfloat t3);
+    void r_s_r_set_matrix(GLfloat s1, GLfloat s2, GLfloat s3, GLfloat r0, GLfloat r1, GLfloat r2, GLfloat r3);
     void paintGL();
     void resizeGL(int width, int height);
 
